38f61007ebd24c5a08d35bc851b961b36a3ba0fe
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2020-05-17 17:59:14 +0200

    enable qbs project to support c++2a and c++2b support
    * also added c++20 and c++17 support for msvc compilers

--- src/plugins/qbsprojectmanager/qbsproject.cpp
+++ src/plugins/qbsprojectmanager/qbsproject.cpp
@@ -794,10 +794,12 @@ static void getExpandedCompilerFlags(QStringList &cFlags, QStringList &cxxFlags,
         cFlags = cxxFlags = commonFlags;
 
         const auto cxxLanguageVersion = arrayToStringList(getCppProp("cxxLanguageVersion"));
-        if (cxxLanguageVersion.contains("c++23"))
+        if (cxxLanguageVersion.contains("c++2b") || cxxLanguageVersion.contains("c++23"))
             cxxFlags << "-std=c++2b";
         else if (cxxLanguageVersion.contains("c++20"))
             cxxFlags << "-std=c++20";
+        else if (cxxLanguageVersion.contains("c++2a"))
+            cxxFlags << "-std=c++2a";
         else if (cxxLanguageVersion.contains("c++17"))
             cxxFlags << "-std=c++17";
         else if (cxxLanguageVersion.contains("c++14"))
@@ -852,10 +854,11 @@ static void getExpandedCompilerFlags(QStringList &cFlags, QStringList &cxxFlags,
         cxxFlags << "/TP";
         if (!enableRtti.isUndefined())
             cxxFlags << QLatin1String(enableRtti.toBool() ? "/GR" : "/GR-");
-        const QJsonArray cxxLanguageVersion = getCppProp("cxxLanguageVersion").toArray();
-        if (cxxLanguageVersion.contains("c++23"))
+
+        const auto cxxLanguageVersion = arrayToStringList(getCppProp("cxxLanguageVersion"));
+        if (cxxLanguageVersion.contains("c++23") || cxxLanguageVersion.contains("c++2b"))
             cxxFlags << "/std:c++latest";
-        else if (cxxLanguageVersion.contains("c++20"))
+        else if (cxxLanguageVersion.contains("c++20") || cxxLanguageVersion.contains("c++2a"))
             cxxFlags << "/std:c++20";
         else if (cxxLanguageVersion.contains("c++17"))
             cxxFlags << "/std:c++17";
