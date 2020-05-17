46a48cb0e59738032b308e339af358cdf18edda3
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2020-05-17 17:59:14 +0200

    enable qbs c++2a support

--- src/plugins/qbsprojectmanager/qbsproject.cpp
+++ src/plugins/qbsprojectmanager/qbsproject.cpp
@@ -786,7 +786,9 @@ static void getExpandedCompilerFlags(QStringList &cFlags, QStringList &cxxFlags,
         cFlags = cxxFlags = commonFlags;
 
         const auto cxxLanguageVersion = arrayToStringList(getCppProp("cxxLanguageVersion"));
-        if (cxxLanguageVersion.contains("c++17"))
+        if (cxxLanguageVersion.contains("c++2a"))
+            cxxFlags << "-std=c++2a";
+        else if (cxxLanguageVersion.contains("c++17"))
             cxxFlags << "-std=c++17";
         else if (cxxLanguageVersion.contains("c++14"))
             cxxFlags << "-std=c++14";
@@ -836,7 +838,9 @@ static void getExpandedCompilerFlags(QStringList &cFlags, QStringList &cxxFlags,
         cxxFlags << "/TP";
         if (!enableRtti.isUndefined())
             cxxFlags << QLatin1String(enableRtti.toBool() ? "/GR" : "/GR-");
-        if (getCppProp("cxxLanguageVersion").toArray().contains("c++17"))
+        if (getCppProp("cxxLanguageVersion").toArray().contains("c++2a"))
+            cxxFlags << "/std:c++latest";
+        else if (getCppProp("cxxLanguageVersion").toArray().contains("c++17"))
             cxxFlags << "/std:c++17";
     }
 }
