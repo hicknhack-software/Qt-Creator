4a8eb5b33a2462def6e44ec9ea27c6bfb28e3c86
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2020-10-24 14:47:51 +0200

    Added Navigation to CompileOutputWindow
    * fixed wrong task positions

--- src/plugins/coreplugin/outputwindow.h
+++ src/plugins/coreplugin/outputwindow.h
@@ -47,6 +47,11 @@ public:
     bool knowsPositionOf(unsigned taskId) const;
     void showPositionOf(unsigned taskId);
 
+    bool hasPositions() const;
+    void goToFirstTaskPosition();
+    void goToNextTaskPosition();
+    void goToPreviousTaskPosition();
+
     void grayOutOldContent();
     void clear();
     void flush();
