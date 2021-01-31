6592c9fa9c851243f6ffde87fd9220887c9eede8
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2021-01-31 03:41:58 +0100

    keep cursor visible after text cut operation

--- src/plugins/texteditor/texteditor.cpp
+++ src/plugins/texteditor/texteditor.cpp
@@ -7634,6 +7634,7 @@ void TextEditorWidget::cut()
     MultiTextCursor cursor = multiTextCursor();
     cursor.removeSelectedText();
     setMultiTextCursor(cursor);
+    ensureCursorVisible();
     d->collectToCircularClipboard();
 }
 
