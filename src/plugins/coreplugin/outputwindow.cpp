cacf069b1c964280e00207db1923ea63150cf6b9
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2021-02-28 03:22:19 +0100

    Suppress scrollToBottom on CompileOutput after navigation

--- src/plugins/coreplugin/outputwindow.cpp
+++ src/plugins/coreplugin/outputwindow.cpp
@@ -167,7 +167,9 @@ OutputWindow::OutputWindow(Context context, const QString &settingsKey, QWidget 
     d->scrollTimer.setInterval(10);
     d->scrollTimer.setSingleShot(true);
     connect(&d->scrollTimer, &QTimer::timeout,
-            this, &OutputWindow::scrollToBottom);
+            this, [&]() {
+        if (d->scrollToBottom) { scrollToBottom(); }
+    });
     d->lastMessage.start();
 
     d->originalFontSize = font().pointSizeF();
@@ -349,6 +351,11 @@ void OutputWindow::updateFilterProperties(
     }
     d->filterMode = flags;
     filterNewContent();
+}
+
+void OutputWindow::disableScrollToBottom()
+{
+    d->scrollToBottom = false;
 }
 
 void OutputWindow::filterNewContent()
