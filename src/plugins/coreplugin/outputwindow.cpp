e62bafa15dfa12e45e219fc261b306a505a69296
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2021-02-28 03:22:19 +0100

    Suppress scrollToBottom on CompileOutput after navigation

--- src/plugins/coreplugin/outputwindow.cpp
+++ src/plugins/coreplugin/outputwindow.cpp
@@ -168,7 +168,9 @@ OutputWindow::OutputWindow(Context context, const Key &settingsKey, QWidget *par
     d->scrollTimer.setInterval(10ms);
     d->scrollTimer.setSingleShot(true);
     connect(&d->scrollTimer, &QTimer::timeout,
-            this, &OutputWindow::scrollToBottom);
+            this, [&]() {
+        if (d->scrollToBottom) { scrollToBottom(); }
+    });
     d->lastMessage.start();
 
     d->originalFontSize = font().pointSizeF();
@@ -726,6 +728,7 @@ void OutputWindow::goToFirstTaskPosition()
     setTextCursor(cursor);
     cursor.movePosition(QTextCursor::StartOfBlock, QTextCursor::KeepAnchor);
     setTextCursor(cursor);
+    d->scrollToBottom = false;
 }
 
 void OutputWindow::goToNextTaskPosition()
@@ -748,6 +751,7 @@ void OutputWindow::goToNextTaskPosition()
     setTextCursor(cursor);
     cursor.movePosition(QTextCursor::StartOfBlock, QTextCursor::KeepAnchor);
     setTextCursor(cursor);
+    d->scrollToBottom = false;
 }
 
 void OutputWindow::goToPreviousTaskPosition()
@@ -770,6 +774,7 @@ void OutputWindow::goToPreviousTaskPosition()
     setTextCursor(cursor);
     cursor.movePosition(QTextCursor::StartOfBlock, QTextCursor::KeepAnchor);
     setTextCursor(cursor);
+    d->scrollToBottom = false;
 }
 
 QMimeData *OutputWindow::createMimeDataFromSelection() const
