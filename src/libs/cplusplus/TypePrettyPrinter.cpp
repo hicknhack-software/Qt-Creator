c97402f13cb00c36f6b1529c8e096bdd23c3ff41
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2021-02-28 01:21:33 +0100

    fixed support for trailing return types on function declaration definition link
    * also support migrating from and to trailing return types

--- src/libs/cplusplus/TypePrettyPrinter.cpp
+++ src/libs/cplusplus/TypePrettyPrinter.cpp
@@ -490,7 +490,7 @@ void TypePrettyPrinter::visit(Function *type)
     argOverview.combineAutoAndName = true;
 
     if (_overview->showReturnTypes) {
-        if (_overview->trailingReturnType) {
+        if (_overview->trailingReturnType || type->isTrailingReturnType()) {
             _text.prepend("auto ");
         } else {
             const QString returnType = retOverview.prettyType(type->returnType());
@@ -588,7 +588,7 @@ void TypePrettyPrinter::visit(Function *type)
         }
     }
 
-    if (_overview->showReturnTypes && _overview->trailingReturnType) {
+    if (_overview->showReturnTypes && (_overview->trailingReturnType || type->isTrailingReturnType())) {
         const QString returnType = retOverview.prettyType(type->returnType());
         if (!returnType.isEmpty())
             _text.append(" -> ").append(returnType);
