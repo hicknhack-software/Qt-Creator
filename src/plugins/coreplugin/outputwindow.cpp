291525d52a80e0551d5226684f7b4ba995d3b23a
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2022-02-05 15:46:54 +0100

    Fix task positions in outputwindow
    * fixed the task offset for tasks created directly by build steps
    * no longer clear output window for new build step
    * update task navigation by outputwindow to capture tasks from output parsers
    * qbs build steps now only report first task for failure
    * qbs build steps do not convert from path to string to path uselessly

--- src/plugins/coreplugin/outputwindow.cpp
+++ src/plugins/coreplugin/outputwindow.cpp
@@ -75,6 +75,7 @@ public:
     QTimer scrollTimer;
     QElapsedTimer lastMessage;
     QHash<unsigned int, QPair<int, int>> taskPositions;
+    int lineCount = 0; // correct line count to apply offsets
     //: default file name suggested for saving text from output views
     QString outputFileNameHint{::Core::Tr::tr("output.txt")};
 };
@@ -514,6 +515,12 @@ void OutputWindow::appendMessage(const QString &output, OutputFormat format)
         d->queuedOutput.last().first.append(output);
     if (!d->queueTimer.isActive())
         d->queueTimer.start();
+
+    d->lineCount += output.count('\n');
+}
+
+int OutputWindow::directTaskOffset() const {
+    return document()->blockCount() - d->lineCount;
 }
 
 void OutputWindow::registerPositionOf(unsigned taskId, int linkedOutputLines, int skipLines,
@@ -527,6 +534,7 @@ void OutputWindow::registerPositionOf(unsigned taskId, int linkedOutputLines, in
     const int lastLine = firstLine + linkedOutputLines - 1;
 
     d->taskPositions.insert(taskId, {firstLine, lastLine});
+    if (d->taskPositions.size() ==1) emit hasPositionsChanged();
 }
 
 bool OutputWindow::knowsPositionOf(unsigned taskId) const
@@ -648,7 +656,10 @@ void OutputWindow::clear()
 {
     d->formatter.clear();
     d->scrollToBottom = true;
+    auto hadTaskPositions = hasPositions();
     d->taskPositions.clear();
+    d->lineCount = 0;
+    if (hadTaskPositions) emit hasPositionsChanged();
 }
 
 void OutputWindow::flush()
@@ -670,8 +681,7 @@ void OutputWindow::reset()
 {
     flush();
     d->queueTimer.stop();
-    d->formatter.reset();
-    d->scrollToBottom = true;
+    clear();
     if (!d->queuedOutput.isEmpty()) {
         d->queuedOutput.clear();
         d->formatter.appendMessage(Tr::tr("[Discarding excessive amount of pending output.]\n"),
