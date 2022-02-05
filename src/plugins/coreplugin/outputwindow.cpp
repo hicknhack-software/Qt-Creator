f3e4bda94b30d78dad9a749e3f7977b96932d99c
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2022-02-05 15:46:54 +0100

    Fix task positions in outputwindow
    * fixed the task offset for tasks created directly by build steps
    * no longer clear output window for new build step
    * update task navigation by outputwindow to capture tasks from output parsers
    * qbs build steps now only report first task for failure
    * qbs build steps do not convert from path to string to path uselessly

--- src/plugins/coreplugin/outputwindow.cpp
+++ src/plugins/coreplugin/outputwindow.cpp
@@ -106,6 +106,7 @@ public:
     QTimer scrollTimer;
     QElapsedTimer lastMessage;
     QHash<unsigned int, QPair<int, int>> taskPositions;
+    int lineCount = 0; // correct line count to apply offsets
 };
 
 } // namespace Internal
@@ -703,6 +704,12 @@ void OutputWindow::appendMessage(const QString &output, OutputFormat format)
         d->queuedOutput.last().first.append(output);
     if (!d->queueTimer.isActive())
         d->queueTimer.start();
+
+    d->lineCount += output.count('\n');
+}
+
+int OutputWindow::directTaskOffset() const {
+    return document()->blockCount() - d->lineCount;
 }
 
 void OutputWindow::registerPositionOf(unsigned taskId, int linkedOutputLines, int skipLines,
@@ -723,6 +730,7 @@ void OutputWindow::registerPositionOf(unsigned taskId, int linkedOutputLines, in
     const int lastLine = firstLine + linkedOutputLines - 1;
 
     d->taskPositions.insert(taskId, {firstLine, lastLine});
+    if (d->taskPositions.size() ==1) emit hasPositionsChanged();
 }
 
 bool OutputWindow::knowsPositionOf(unsigned taskId) const
@@ -844,8 +852,11 @@ void OutputWindow::clear()
 {
     d->formatter.clear();
     d->scrollToBottom = true;
+    auto hadTaskPositions = hasPositions();
     d->taskPositions.clear();
     d->startOfNewContentCursor.setPosition(0);
+    d->lineCount = 0;
+    if (hadTaskPositions) emit hasPositionsChanged();
 }
 
 void OutputWindow::clearLinesPrefixedWith(const QString& prefix, bool deleteTrailingLineBreak)
@@ -883,6 +894,8 @@ void OutputWindow::flush()
 void OutputWindow::reset()
 {
     flush();
+    d->queueTimer.stop();
+    clear();
     if (!d->queuedOutput.isEmpty()) {
         discardPendingToolOutput();
         flush();
