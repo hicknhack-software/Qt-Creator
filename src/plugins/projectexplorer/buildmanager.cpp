92183d18306db97445856e6f70d4a7773b602bbb
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2023-06-10 23:57:28 +0200

    added long build media playback support

--- src/plugins/projectexplorer/buildmanager.cpp
+++ src/plugins/projectexplorer/buildmanager.cpp
@@ -414,6 +414,20 @@ public:
 static BuildManagerPrivate *d = nullptr;
 static BuildManager *m_instance = nullptr;
 
+static void handleLongBuild(bool success)
+{
+    if (std::chrono::milliseconds{d->m_elapsed.elapsed()} <= std::chrono::seconds{
+            globalProjectExplorerSettings().longBuildThreshold.value()}) {
+        return;
+    }
+    auto media = success
+                     ? globalProjectExplorerSettings().longBuildSuccessMediaPath.value()
+                     : globalProjectExplorerSettings().longBuildFailedMediaPath.value();
+    if (media.isEmpty())
+        return;
+    ProjectExplorerSettings::playAlertMedia(media);
+}
+
 BuildManager::BuildManager(QObject *parent, QAction *cancelBuildAction)
     : QObject(parent)
 {
@@ -858,6 +872,9 @@ void BuildManager::startBuildQueue()
             d->m_buildQueue = d->m_pendingQueue;
             d->m_pendingQueue.clear();
             startBuildQueue();
+        }
+        else {
+            handleLongBuild(success);
         }
     };
 
