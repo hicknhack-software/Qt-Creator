98f837e2b0f3b2a870bfabbf8af92dabcdff82ae
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2026-04-11 01:56:49 +0200

    Attempted Fix: Limit running QbsSessions to active configurations

--- src/plugins/qbsprojectmanager/qbssession.h
+++ src/plugins/qbsprojectmanager/qbssession.h
@@ -110,6 +110,8 @@ public:
 
     int apiLevel() const;
 
+    void shutdown();
+
     void sendRequest(const QJsonObject &request);
     void cancelCurrentJob();
     void requestFilesGeneratedFrom(const QHash<QString, QStringList> &sourceFilesPerProduct);
@@ -174,7 +176,6 @@ private:
     QString protocolErrorMsg() const;
     QString qbsExecutableUserString() const;
     void setError(const QString &error);
-    void setInactive();
     FileChangeResult updateFileList(
         const char *action,
         const std::variant<QStringList, QList<std::pair<QString, QString>>> &files,
